15d50fcb3da33f61fb41207e727f3f48d22e2386
excaliburxyz <fefef8577@gmail.com>
2024-08-01 10:44:22 +0700

    delete number

--- main.c
+++ main.c
@@ -9,33 +9,33 @@ void persiapanBahan() {
 // Fungsi untuk menumis bumbu
 void menumisBumbu() {
     printf("Menumis Bumbu:\n");
-    printf("1. Tumis bawang putih dan bawang merah hingga harum.\n");
-    printf("2. Tambahkan cabai dan tumis lagi.\n\n");
+    printf("    Tumis bawang putih dan bawang merah hingga harum.\n");
+    printf("    Tambahkan cabai dan tumis lagi.\n\n");
 }
 
 // Fungsi untuk menambahkan telur
 void menambahkanTelur() {
     printf("Menambahkan Telur:\n");
-    printf("1. Masukkan telur ke dalam wajan dan aduk hingga matang.\n\n");
+    printf("    Masukkan telur ke dalam wajan dan aduk hingga matang.\n\n");
 }
 
 // Fungsi untuk memasak nasi
 void memasakNasi() {
     printf("Memasak Nasi:\n");
-    printf("1. Masukkan nasi matang ke dalam wajan dan aduk hingga merata.\n\n");
+    printf("    Masukkan nasi matang ke dalam wajan dan aduk hingga merata.\n\n");
 }
 
 // Fungsi untuk menambahkan bumbu dan sayuran
 void menambahkanBumbuDanSayuran() {
     printf("Menambahkan Bumbu dan Sayuran:\n");
-    printf("1. Tambahkan kecap, garam, dan sayuran ke dalam wajan.\n");
-    printf("2. Aduk rata dan masak hingga sayuran matang dan bumbu meresap.\n\n");
+    printf("    Tambahkan kecap, garam, dan sayuran ke dalam wajan.\n");
+    printf("    Aduk rata dan masak hingga sayuran matang dan bumbu meresap.\n\n");
 }
 
 // Fungsi untuk menyajikan nasi goreng
 void menyajikan() {
     printf("Menyajikan:\n");
-    printf("Angkat nasi goreng dari wajan dan sajikan di atas piring.\n");
+    printf("    Angkat nasi goreng dari wajan dan sajikan di atas piring.\n");
 }
 
 
